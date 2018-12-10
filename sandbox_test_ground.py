528765a2aa2b2eac11af8a9e0ffddaf704fe7c67
Jordan <JordanChell@vivaldi.net>
2018-12-10 21:29:57 +0000

    testing the room_name function to test that attribte doesn't exist error is gone

--- sandbox_test_ground.py
+++ sandbox_test_ground.py
@@ -1,31 +1,5 @@
-class Player(object):
-    def __init__(self):
-        self.x = 2
-        self.y = 2
-        self.inventory = {"map":"y", "barrel":"n"}
+import rooms
 
-    def current_inventory(self, inventory, item):
-        item = item.lower()
-        if item in self.inventory:
-            if self.inventory[item] == "n":
-                self.inventory[item] == "y"
-                print("you now have access to " + item)
-            elif self.inventory[item] == "y":
-                print("you already have access to that item!")
+current_room = rooms.room8()
 
-
-    def map(self):
-        if self.inventory["map"] == "n":
-            print("You currently do not have access to the map sorry!")
-            return None
-        else:
-            print("You have access to the map!")
-
-        print(self.x, self.y)
-
-
-
-x = Player()
-x.inventory["map"] == "n"
-x.current_inventory(x.inventory,"map")
-x.map()
+print(current_room.room_name())
