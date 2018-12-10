cb0286ee98410fb6e08dfa2080274bce087d1232
Jordan <JordanChell@vivaldi.net>
2018-12-10 21:06:27 +0000

    added a skeleton code version of an inventory

--- player.py
+++ player.py
@@ -2,6 +2,13 @@ class Player(object):
     def __init__(self):
         self.x = 2
         self.y = 2
+        self.inventory = {"map":"y", "barrel":"n"}
 
-    def inventory(self):
-        inventory = []
+    def current_inventory(self, inventory, item):
+        if item in self.inventory:
+            if self.inventory[item] == "n":
+                self.inventory[item] == "y"
+                print("you now have access to " + item)
+
+x = Player()
+x.current_inventory(x.inventory,"barrel")
