064379f30ef734856c9e32025ea9d967c6ea032e
Jordan <JordanChell@vivaldi.net>
2018-12-10 21:21:13 +0000

    testing for player inventory

--- sandbox_test_ground.py
+++ sandbox_test_ground.py
@@ -1,8 +1,31 @@
-def update_room():
-    for x in range(1,4):
-        for y in range(1,4):
-            print(x,y)
+class Player(object):
+    def __init__(self):
+        self.x = 2
+        self.y = 2
+        self.inventory = {"map":"y", "barrel":"n"}
+
+    def current_inventory(self, inventory, item):
+        item = item.lower()
+        if item in self.inventory:
+            if self.inventory[item] == "n":
+                self.inventory[item] == "y"
+                print("you now have access to " + item)
+            elif self.inventory[item] == "y":
+                print("you already have access to that item!")
 
 
-update_room()
-#testing room searching method
+    def map(self):
+        if self.inventory["map"] == "n":
+            print("You currently do not have access to the map sorry!")
+            return None
+        else:
+            print("You have access to the map!")
+
+        print(self.x, self.y)
+
+
+
+x = Player()
+x.inventory["map"] == "n"
+x.current_inventory(x.inventory,"map")
+x.map()
